fd36eea3f59e8d3da0435c2548f13eecf3f616c2
Christopher Bruns <cmbruns@rotatingpenguin.com>
2017-03-09 13:39:41 -0500

    Paint screen quad using shader.

--- globeview.js
+++ globeview.js
@@ -1,18 +1,77 @@
 "use strict";
 
 var gl = null;
+var vao = null;
+var program = null;
+
+var vertexShader = `#version 300 es
+
+#line 10
+const vec4 SCREEN_QUAD[4] = vec4[4](
+  vec4(-1, -1, 0.5, 1),
+  vec4( 1, -1, 0.5, 1),
+  vec4(-1,  1, 0.5, 1),
+  vec4( 1,  1, 0.5, 1));
+
+void main() {
+  gl_Position = SCREEN_QUAD[gl_VertexID];
+}
+`;
+
+var fragmentShader = `#version 300 es
+
+#line 24
+precision mediump float;
+out vec4 outColor;
+
+void main() {
+  outColor = vec4(0, 0, 1, 1);
+}
+`;
 
 function initGL(canvas) {
-  gl = canvas.getContext("webgl");
+  gl = canvas.getContext("webgl2");
   if (!gl) {
     alert("Could not initialise WebGL, sorry :-(");
     return;
+  }
+  vao = gl.createVertexArray(); // webgl2 only
+  gl.bindVertexArray(vao);
+  var vs = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vs, vertexShader);
+  gl.compileShader(vs);
+  // Check if it compiled
+  var success = gl.getShaderParameter(vs, gl.COMPILE_STATUS);
+  if (!success) {
+    // Something went wrong during compilation; get the error
+    throw "could not compile shader:" + gl.getShaderInfoLog(vs);
+  }
+  var fs = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fs, fragmentShader);
+  gl.compileShader(fs);
+  // Check if it compiled
+  success = gl.getShaderParameter(fs, gl.COMPILE_STATUS);
+  if (!success) {
+    // Something went wrong during compilation; get the error
+    throw "could not compile shader:" + gl.getShaderInfoLog(fs);
+  }
+  program = gl.createProgram();
+  gl.attachShader(program, vs);
+  gl.attachShader(program, fs);
+  gl.linkProgram(program);
+  success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!success) {
+      // something went wrong with the link
+      throw ("program filed to link:" + gl.getProgramInfoLog (program));
   }
 }
 
 function drawScene() {
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.bindVertexArray(vao);
+  gl.useProgram(program);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
 
 function globeviewStart() {
@@ -22,4 +81,3 @@ function globeviewStart() {
   gl.disable(gl.DEPTH_TEST);
   drawScene();
 }
-
